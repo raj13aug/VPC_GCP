4c5b609f6b11d7b08274deeffc3b980ee4626e3f
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-11-30 13:15:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "google_compute_router_nat" "router_nat" {
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
   subnetwork {
-    name                    = "${var.name}-${local.type[1]}-subnetwork"
+    name                    = "${var.name}-${local.type[0]}-subnetwork"
     source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
   }
 }
